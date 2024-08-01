4729c386a2848fb4fa87030f6148c511197ef915
Gil Nunes <gil.nunes@ctw.bmwgroup.com>
2024-08-01 22:54:33 +0100

    basic nvim

--- init.lua
+++ init.lua
@@ -1,53 +1,128 @@
-require "core"
-
-local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
-
-if custom_init_path then
-  dofile(custom_init_path)
-end
-
-require("core.utils").load_mappings()
-
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-
--- bootstrap lazy.nvim!
+-- Set leader key early
+vim.g.mapleader = " "
+-- Ensure lazy.nvim is installed
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  require("core.bootstrap").gen_chadrc_template()
-  require("core.bootstrap").lazy(lazypath)
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
-
--- dofile(vim.g.base46_cache .. "defaults")
 vim.opt.rtp:prepend(lazypath)
-require "plugins"
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+-- Load lazy.nvim
+require("lazy").setup({
+{
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' }
+},
+    {'sbdchd/neoformat'},
+  -- Telescope and its dependencies
+  {
+    'nvim-telescope/telescope.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  -- Tree-sitter and its dependencies
+  {
+    'nvim-treesitter/nvim-treesitter',
+    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+  },
+{'akinsho/toggleterm.nvim', version = "*", config = true},
+{
+    "williamboman/mason.nvim"
+},
+    {'nvim-tree/nvim-tree.lua'},
+    {'xiyaowong/transparent.nvim'},
+    {'olivercederborg/poimandres.nvim',
+    config = function()
+        -- Set the poimandres color scheme
+        -- vim.cmd('colorscheme poimandres')
+    end},
+	 {
+	   "rockyzhang24/arctic.nvim",
+	   dependencies = { "rktjmp/lush.nvim" },
+	   name = "arctic",
+	   branch = "v2",
+	   priority = 1000,
+	   config = function()
+	     vim.cmd("colorscheme arctic")
+	   end
+	 },
 
--- greatest remap ever
-vim.keymap.set("x", "<leader>p", [["_dP]])
+	{'tpope/vim-commentary'},
+    {'mg979/vim-visual-multi'},
+{
+  "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+  },
+  keys = {
+    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  },
+}
+})
 
--- next greatest remap ever : asbjornHaland
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+require("transparent").setup({ -- Optional, you don't have to run setup.
+  groups = { -- table: default groups
+    'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+    'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+    'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+    'SignColumn', 'CursorLine', 'CursorLineNr', 'StatusLine', 'StatusLineNC',
+    'EndOfBuffer',
+  },
+  extra_groups = {}, -- table: additional groups that should be cleared
+  exclude_groups = {}, -- table: groups you don't want to clear
+})
+-- Telescope configuration
+require('telescope').setup{
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-h>"] = "which_key"
+      }
+    }
+  }
+}
+-- Tree-sitter configuration
+require('nvim-treesitter.configs').setup {
+  ensure_installed = { "c","cpp", "lua", "python"}, -- List of parsers to install
+  sync_install = false,
+  highlight = {
+    enable = true,
+    disable = {},
+  },
+  indent = {
+    enable = true
+  },
+}
+-- Unmap <C-p> in normal, visual, and insert modes
+vim.api.nvim_set_keymap('n', '<C-p>', '', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<C-p>', '', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<C-p>', '', { noremap = true, silent = true })
+-- Unmap <C-f> in normal, visual, and insert modes
+vim.api.nvim_set_keymap('n', '<C-f>', '', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<C-f>', '', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<C-f>', '', { noremap = true, silent = true })
 
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+-- Keybindings for Telescope
+vim.api.nvim_set_keymap('n', '<C-p>', '<cmd>Telescope find_files<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-f>', '<cmd>Telescope live_grep<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<CR>', { noremap = true, silent = true })
 
--- This is going to get me cancelled
-vim.keymap.set("i", "<C-c>", "<Esc>")
 
-vim.keymap.set("n", "<leader>R", "<cmd>RustRun<CR>")
-vim.keymap.set("n", "<leader>T", "<cmd>RustTest!<CR>")
-vim.keymap.set("n", "Q", "<nop>")
-
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
@@ -62,7 +137,6 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
@@ -76,86 +150,139 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
-
 -- search settings
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
--- clipboard
-vim.opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 
+vim.cmd("set nofixendofline")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- clipboard
+-- Enable clipboard support
+vim.opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+vim.api.nvim_set_option("clipboard", "unnamedplus")
 -- split windows
 vim.opt.splitright = true -- split vertical window to the right
-vim.opt.splitbelow = true -- split horizontal window to the bottom 
-vim.opt.cursorline = true
+vim.opt.splitbelow = true -- split horizontal window to the bottom
 
-require("cmake-tools").setup {
-  cmake_command = "cmake", -- this is used to specify cmake command path
-  cmake_regenerate_on_save = true, -- auto generate when save CMakeLists.txt
-  cmake_generate_options = { "-DCMAKE_EXPORT_COMPILE_COMMANDS=1" }, -- this will be passed when invoke `CMakeGenerate`
-  cmake_build_options = {}, -- this will be passed when invoke `CMakeBuild`
-  cmake_build_directory = "", -- this is used to specify generate directory for cmake
-  cmake_build_directory_prefix = "cmake_build_", -- when cmake_build_directory is set to "", this option will be activated
-  cmake_soft_link_compile_commands = true, -- this will automatically make a soft link from compile commands file to project root dir
-  cmake_compile_commands_from_lsp = false, -- this will automatically set compile commands file location using lsp, to use it, please set `cmake_soft_link_compile_commands` to false
-  cmake_kits_path = nil, -- this is used to specify global cmake kits path, see CMakeKits for detailed usage
-  cmake_variants_message = {
-    short = { show = true }, -- whether to show short message
-    long = { show = true, max_length = 40 }, -- whether to show long message
-  },
-  cmake_dap_configuration = { -- debug settings for cmake
-    name = "cpp",
-    type = "codelldb",
-    request = "launch",
-    stopOnEntry = false,
-    runInTerminal = true,
-    console = "integratedTerminal",
-  },
-  cmake_executor = { -- executor to use
-    name = "quickfix", -- name of the executor
-    opts = {}, -- the options the executor will get, possible values depend on the executor type. See `default_opts` for possible values.
-    default_opts = { -- a list of default and possible values for executors
-      quickfix = {
-        show = "always", -- "always", "only_on_error"
-        position = "belowright", -- "bottom", "top"
-        size = 10,
-      },
-      overseer = {
-        new_task_opts = {}, -- options to pass into the `overseer.new_task` command
-        on_new_task = function(task) end, -- a function that gets overseer.Task when it is created, before calling `task:start`
-      },
-      terminal = {}, -- terminal executor uses the values in cmake_terminal
-    },
-  },
-  cmake_terminal = {
-    name = "terminal",
-    opts = {
-      name = "Main Terminal",
-      prefix_name = "[CMakeTools]: ", -- This must be included and must be unique, otherwise the terminals will not work. Do not use a simple spacebar " ", or any generic name
-      split_direction = "horizontal", -- "horizontal", "vertical"
-      split_size = 11,
+-- Map Ctrl-Shift-c to copy to the system clipboard in visual mode
+vim.api.nvim_set_keymap('v', '<C-c>', '"+y', { noremap = true, silent = true })
 
-      -- Window handling
-      single_terminal_per_instance = true, -- Single viewport, multiple windows
-      single_terminal_per_tab = true, -- Single viewport per tab
-      keep_terminal_static_location = true, -- Static location of the viewport if avialable
+-- Map Ctrl-Shift-v to paste from the system clipboard in normal mode
+vim.api.nvim_set_keymap('n', '<C-S-v>', '"+p', { noremap = true, silent = true })
 
-      -- Running Tasks
-      start_insert_in_launch_task = false, -- If you want to enter terminal with :startinsert upon using :CMakeRun
-      start_insert_in_other_tasks = false, -- If you want to enter terminal with :startinsert upon launching all other cmake tasks in the terminal. Generally set as false
-      focus_on_main_terminal = false, -- Focus on cmake terminal when cmake task is launched. Only used if executor is terminal.
-      focus_on_launch_terminal = false, -- Focus on cmake launch terminal when executable target in launched.
-    },
-  },
-  cmake_notifications = {
-    enabled = true, -- show cmake execution progress in nvim-notify
-    spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }, -- icons used for progress display
-    refresh_rate_ms = 100, -- how often to iterate icons
-  },
+-- Map Ctrl-Shift-v to paste from the system clipboard in visual mode
+vim.api.nvim_set_keymap('v', '<C-S-v>', '"+p', { noremap = true, silent = true })
+
+-- Map Ctrl-\ to split the window horizontally
+vim.api.nvim_set_keymap('n', '<C-\\>', ':vsplit<CR>', { noremap = true, silent = true })
+
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('highlight_yank', {}),
+  desc = 'Hightlight selection on yank',
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 250 }
+  end,
+})
+-- Then configure the keybinding
+require("toggleterm").setup{
+  open_mapping = [[<A-j>]],
+ direction = 'horizontal', -- or 'horizontal' or 'vertical'
+  size = 20,
+  -- you can add other configurations here
 }
--- vim.cmd "colorscheme darcula-dark"
--- Lua
-vim.cmd("colorscheme arctic")
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-vim.g.dap_virtual_text = true
+
+-- Map Ctrl + c to Ctrl + \ Ctrl + n in terminal mode
+vim.api.nvim_set_keymap('t', '<C-h>', '<C-\\><C-n>:wincmd h<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<C-j>', '<C-\\><C-n>:wincmd j<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<C-k>', '<C-\\><C-n>:wincmd k<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<C-l>', '<C-\\><C-n>:wincmd h<CR>', { noremap = true, silent = true })
+require("mason").setup()
+
+
+
+
+-- Configure neoformat
+vim.g.neoformat_enabled_cpp = { "clangformat" }
+
+-- Run neoformat on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.c", "*.cpp", "*.h", "*.hpp", "*.cc", "*.hh" },
+  command = "Neoformat"
+})
+
+-- Enable undo files
+vim.opt.undofile = true
+
+-- Specify the directory for undo files (optional)
+vim.opt.undodir = vim.fn.expand("~/.config/nvim/undo")
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+
+-- Require the nvim-tree plugin
+local nvim_tree = require('nvim-tree')
+
+-- Setup nvim-tree (optional, you can customize settings here)
+nvim_tree.setup { }
+
+-- Define the key mapping for toggling Nvim Tree
+vim.api.nvim_set_keymap('n', '<C-e>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+-- Define the key mapping for toggling Nvim Tree
+vim.api.nvim_set_keymap('n', '<C-b>', ':NvimTreeClose<CR>', { noremap = true, silent = true })
+
+
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'arctic',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
